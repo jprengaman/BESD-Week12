a60ed8438252705a27c93f78dde04e4041cb93b2
John Prengaman <prengaman78@gmail.com>
2022-07-22 15:58:32 -0700

    Add files via upload

---
+++ TestDemo.java
@@ -0,0 +1,24 @@
+package com.promineotech;
+import java.util.Random;
+
+public class TestDemo {
+
+  public int addPositive (int a, int b) {
+    if(a > 0 && b > 0) {
+      return a + b;
+    }
+    else {
+      throw new IllegalArgumentException("Both parameters must be positive!");
+    }
+  }
+  
+  public int randomNumberSquared() {
+    return (getRandomInt()) * (getRandomInt());
+  }
+
+  public int getRandomInt() {
+    Random random = new Random();
+    return random.nextInt(10) + 1;
+  }
+  
+}
